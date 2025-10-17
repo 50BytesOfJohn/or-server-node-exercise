154becf1b46f6cd9f6d6b333ad2ab0fd29e83266
Tomasz Szczuciński <tomasz.szczucinski@mynth.io>
2025-10-17 18:26:03 +0200

    feat: DB schema update
    * Initial DB changes for basic requested model

---
+++ src/db/migrations/0000_conscious_red_wolf.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "orders" (
+	"id" varchar(25) PRIMARY KEY NOT NULL,
+	"order_date" timestamp DEFAULT now() NOT NULL,
+	"total_amount" numeric(10, 2) NOT NULL,
+	"user_id" uuid NOT NULL,
+	"organization_id" uuid NOT NULL,
+	"date_updated" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "organizations" (
+	"id" varchar(25) PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"industry" text NOT NULL,
+	"date_founded" date NOT NULL,
+	"date_created" timestamp DEFAULT now() NOT NULL,
+	"date_updated" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" varchar(25) PRIMARY KEY NOT NULL,
+	"first_name" text NOT NULL,
+	"last_name" text NOT NULL,
+	"email" text NOT NULL,
+	"organization_id" uuid,
+	"date_created" timestamp DEFAULT now() NOT NULL,
+	"date_updated" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "idx_users_organization_id" ON "users" USING btree ("organization_id");
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,68 @@
 import { pgEnum, pgTable as table } from "drizzle-orm/pg-core";
 import * as t from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 
-// TODO: Add schema
+export const usersTable = table(
+  "users",
+  {
+    id: t
+      .varchar("id", { length: 25 })
+      .primaryKey()
+      .$defaultFn(() => `usr_${nanoid(21)}`),
 
-export const usersTable = table("users", {
-  id: t.uuid("id").primaryKey().defaultRandom(),
+    firstName: t.text("first_name").notNull(),
+    lastName: t.text("last_name").notNull(),
+
+    email: t.text("email").notNull().unique(),
+
+    organizationId: t
+      .varchar("organization_id", { length: 25 })
+      .references(() => organizationsTable.id)
+      .notNull(),
+
+    dateCreated: t.timestamp("date_created").defaultNow().notNull(),
+    dateUpdated: t.timestamp("date_updated").defaultNow().notNull(),
+  },
+  (table) => [t.index("idx_users_organization_id").on(table.organizationId)]
+);
+
+export const organizationsTable = table("organizations", {
+  id: t
+    .varchar("id", { length: 25 })
+    .primaryKey()
+    .$defaultFn(() => `org_${nanoid(21)}`),
+
+  name: t.text("name").notNull(),
+
+  // I would suggest adding a slug field here for better UX with path or subdomains
+  // slug: t.text("slug").notNull().unique(),
+
+  industry: t.text("industry").notNull(),
+
+  dateFounded: t.date("date_founded").notNull(),
+
+  dateCreated: t.timestamp("date_created").defaultNow().notNull(),
+  dateUpdated: t.timestamp("date_updated").defaultNow().notNull(),
+});
+
+export const ordersTable = table("orders", {
+  id: t
+    .varchar("id", { length: 25 })
+    .primaryKey()
+    .$defaultFn(() => `ord_${nanoid(21)}`),
+
+  orderDate: t.timestamp("order_date").defaultNow().notNull(),
+
+  totalAmount: t.decimal("total_amount", { precision: 10, scale: 2 }).notNull(),
+
+  userId: t
+    .varchar("user_id", { length: 25 })
+    .references(() => usersTable.id)
+    .notNull(),
+  organizationId: t
+    .varchar("organization_id", { length: 25 })
+    .references(() => organizationsTable.id)
+    .notNull(),
+
+  dateUpdated: t.timestamp("date_updated").defaultNow().notNull(),
 });
