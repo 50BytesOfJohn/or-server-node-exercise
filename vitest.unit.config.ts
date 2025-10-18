25bb4d0312ae51a080a30fbaf2b4aff4ccef16a7
Tomasz Szczuciński <tomasz.szczucinski@mynth.io>
2025-10-18 20:14:40 +0200

    chore: add default env for unit tests

--- vitest.unit.config.ts
+++ vitest.unit.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
     include: ["src/**/*.unit.test.ts"],
     globals: true,
     environment: "node",
+    env: {
+      NODE_ENV: "test",
+      DISABLE_CACHE: "true",
+      DATABASE_URL: "postgresql://1:1@localhost:5432/DUMMY",
+      REDIS_URL: "redis://redis:6379",
+      JWT_SECRET: "test",
+      LOG_LEVEL: "debug",
+    },
   },
 });
